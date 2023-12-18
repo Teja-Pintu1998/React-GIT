d498d6a75e461eb9347fa58e870dff3f7c5dc6d3
Teja-Pintu1998 <saitejagunjala@gmail.com>
2023-12-17 18:34:10 -0600

    ep-01

---
+++ App.js
@@ -0,0 +1,4 @@
+const heading = [React.createElement("div",{id:"p1"},React.createElement("h2",{id:"p2"},"Hello World0")),React.createElement("h1",{id:"p3"},"Hello World2")];
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+console.log(heading);
